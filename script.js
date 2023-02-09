718544210a805edcc890ad82d7adcd1529d25bca
Brain Atoms <badams6453@gmail.com>
2023-02-09 18:01:19 -0500

    changed Javascript to make password length of user choice

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
 // Assignment code here
-function writePassword() {
-  var password = generatePassword() 
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-  }
+chars = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
 
-  askUser = function () {
-    var userChoice = window.prompt ("How many characters would you like your password to contain? (Max=12, Min=8)") 
-    if (!userChoice) {
-      return; 
-    } 
-    {
-    window.confirm ("Click OK to confirm including special characters.")
-    window.confirm ("Click OK to confirm including numeric characters.")
-    window.confirm ("Click OK to confirm including lowercase characters.")
-    window.confirm ("Click OK to confirm including uppercase characters.")
-  }
-  writePassword()
-  }
+// askUser = function () {
+//   var userChoice = window.prompt ("How many characters would you like your password to contain? (Max=12, Min=8)") 
+//   if (!userChoice) {
+//     return 
+//   } 
+//   {
+//   var specialChar = window.confirm ("Click OK to confirm including special characters.")
+//   if (!specialChar) {chars= '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'}
+//   var numChar = window.confirm ("Click OK to confirm including numeric characters.")
+//   if (!numChar) { chars= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'}
+//   var lowChar = window.confirm ("Click OK to confirm including lowercase characters.")
+//   if (!lowChar) {chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'}
+//   var uppChar = window.confirm ("Click OK to confirm including uppercase characters.")
+//   if (!uppChar) {chars = '0123456789abcdefghijklmnopqrstuvwxyz'}
+//   else {chars = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';}
+// }
+// writePassword()
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", askUser, writePassword);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  chars = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
-  var passwordLength = 12; 
+
+
+var userChoice = window.prompt ("How many characters would you like your password to contain? (Max=12, Min=8)"); 
+if (!userChoice) {
+  return } else 
+var specialChar = window.confirm ("Click OK to confirm including special characters.");
+var numChar = window.confirm ("Click OK to confirm including numeric characters.")
+var lowChar = window.confirm ("Click OK to confirm including lowercase characters.")
+var uppChar = window.confirm ("Click OK to confirm including uppercase characters.")
+  {
+  var passwordLength = userChoice;
   var password = "";
   for (i = 0; i< passwordLength; i++) {
     var randomNumer = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumer, randomNumer + 1);
-  }
+  } }
 
   document.getElementById("password").value = password;
+    }
 
-  generatePassword()
-} 
+function writePassword() {
+  var password = generatePassword(); 
+  }
+
+
+generateBtn.addEventListener("click", writePassword);
+
+
 
 // var passwordText = document.querySelector("#password");
 
