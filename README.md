5b590d76c6157d4572fe890cd133974eebefb199
Brain Atoms <badams6453@gmail.com>
2023-02-10 15:16:28 -0500

    added comments to Javascript file and updated README

---
+++ README.md
@@ -0,0 +1,24 @@
+# Password_Generator 
+
+The Password_Generator app allows users to randomly generate new password they can use according to their own specifications. The password generator page is simple and user-friendly allowing users to simply click on a button, answer a few questions and then have a new password displayed for them. This web page uses HTML, CSS and Javascript to appear neat, easy to understand and also easy to interact with. 
+
+# ![Screen Shot 2023-02-10 at 3 12 38 PM (2)](https://user-images.githubusercontent.com/61917285/218189021-a8e93fa8-8655-4c91-9e7e-0e22458872a4.png)
+
+#
+[Password Generator](https://github.com/BrainAtoms/Password_Generator)
+
+## Installation
+
+N/A
+
+## Usage
+
+The Password Generator Site can be used for people to obtain a new password that they can then use for an e-mail account or for registration onto all types of websites. Once clicked, the generate button will ask what character types users would like to be included in their new password. The password will then be designed and displayed according to their specifications after they have entered all their criteria. 
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -3,14 +3,17 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
+//Obtains user input and creates a password according to their specifications
+function writePassword() {
   var userChoice = window.prompt(
     "How many characters would you like your password to contain? (Max=15, Min=8)"
   );
+  //returns to home screen if user fails to select a number
   if (isNaN(userChoice)) {
    window.alert("You must enter a number.");
    return; 
   }
+  //sets limits for user's password length
   if (userChoice < 8) {
     window.alert("That number is too low.");
     return;
@@ -22,6 +25,7 @@ function generatePassword() {
   if (!userChoice) {
     return;
   }
+  //asks user's preferences for desired password and only adds confirmed characters
   else 
   var chars = "";
   var specialChar = window.confirm(
@@ -48,10 +52,12 @@ function generatePassword() {
   if (uppChar) {
     chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
+  //returns user to home screen if no character type is chosen
   if (!specialChar && !numChar && !lowChar && !uppChar) {
     window.alert("You must select at least one character type");
     return;
   }
+  //creates random password according to previous user inputs and stores into a password variable
   {
     var passwordLength = userChoice;
     var password = "";
@@ -60,15 +66,17 @@ function generatePassword() {
       password += chars.substring(randomNumer, randomNumer + 1);
     }
   }
-
+//value of the password id becomes what is stored into password variable 
   document.getElementById("password").value = password;
 }
 
-function writePassword() {
-  var password = generatePassword();
-}
+
+// function writePassword() {
+//   var password = generatePassword();
+// }
 
 // Add event listener to generate button
+//displays password in HTML textarea when generate button is clicked
 generateBtn.addEventListener("click", writePassword);
 
 // var passwordText = document.querySelector("#password");
